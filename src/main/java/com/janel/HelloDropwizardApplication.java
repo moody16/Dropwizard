9bfad9d5521985435df0dd025d9c8b594de188eb
moody16 <janek@MacBook-Pro-Occ.local>
2018-10-18 09:17:25 +0200

    Initial commit

---
+++ src/main/java/com/janel/HelloDropwizardApplication.java
@@ -0,0 +1,29 @@
+package com.janel;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloDropwizardApplication extends Application<HelloDropwizardConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new HelloDropwizardApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "HelloDropwizard";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<HelloDropwizardConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final HelloDropwizardConfiguration configuration,
+                    final Environment environment) {
+        // TODO: implement application
+    }
+
+}
---
+++ src/main/java/com/janel/HelloDropwizardConfiguration.java
@@ -0,0 +1,10 @@
+package com.janel;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.*;
+import javax.validation.constraints.*;
+
+public class HelloDropwizardConfiguration extends Configuration {
+    // TODO: implement service configuration
+}
